aeb9ce142e9ea6828e8f495b009b5e7c7e0a57ff
muslabz <ajetunmobimust3@gmail.com>
2023-01-24 02:58:50 -0800

    asdfgfd

---
+++ 104-heap_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * swap - two values
+ * @array: array to sort
+ * @x: first val
+ * @y: second val
+ * @size: size of array
+ * Return: nothing
+ */
+void swap(int *array, int *x, int *y, size_t size)
+{
+	int t
